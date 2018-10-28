1221d7111c8ff76b0cc9e082c30eb91484fd4f8c
yowko.tsai <yowko.tsai@johnsontechinc.com>
2018-10-28 23:53:03 +0800

    enable windows cmd autocompletion

---
+++ enableautocomplete.ps1
@@ -0,0 +1,2 @@
+﻿Set-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Command Processor" -Name CompletionChar -Value 9
+Set-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Command Processor" -Name PathCompletionChar -Value 9
